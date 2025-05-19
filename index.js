695aaa9fba60fcf6a9687b49dffbed37a5d71e1e
Faysal2123 <foysal2005n1@gmail.com>
2025-05-19 22:15:04 +0600

    Add GET endpoint to fetch all courses from database

---
+++ index.js
@@ -0,0 +1,54 @@
+const express =require('express')
+const cors =require('cors')
+require('dotenv').config()
+const app =express()
+const port=process.env.PORT|| 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.erebr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const database=client.db('E-Course')
+    const courseCollection=database.collection('course')
+    const reviewCollection=database.collection('reviews')
+    const userCollection=database.collection('users')
+    const bookedCollection=database.collection('booked')
+
+    app.get('/course',async(req,res)=>{
+        const courses=await courseCollection.find().toArray()
+        res.send(courses)
+    })
+  
+
+
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+app.get('/',(req,res)=>{
+    res.send("Hello from server")
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
+
+
